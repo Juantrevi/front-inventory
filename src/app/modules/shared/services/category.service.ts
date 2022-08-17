50d5087343010d277da735fdd6081bee12c8db2f
Juan Treviranus <juantrevi70@gmail.com>
2022-08-17 18:26:15 -0300

    Buscar Categoria por id

--- src/app/modules/shared/services/category.service.ts
+++ src/app/modules/shared/services/category.service.ts
@@ -35,4 +35,10 @@ export class CategoryService {
     const endpoint = `${base_url}/categories/${id}`;
     return this.http.delete(endpoint);
   }
+
+  //find by id
+    getCategoryById(id: any){
+    const endpoint = `${base_url}/categories/${id}`;
+    return this.http.get(endpoint);
+}
 }
